918bddcd0a28d6327f33012626782eec12ac2446
VakoKobulashvili <itzvakkoofficial@gmail.com>
2026-02-26 21:10:57 +0400

    made controllers async and finished post

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -13,12 +13,35 @@ const tasks = [
   },
 ];
 
-router.get("/", (req, res) => {
-  const data = tasks;
-  res.json(data);
+router.get("/", async (req, res) => {
+  try {
+    const data = tasks;
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json("Error while fetching users", err);
+  }
 });
 
-// router.post("/");
+router.post("/", async (req, res) => {
+  try {
+    const body = req.body;
+
+    const newTask = {
+      id: tasks.length + 1,
+      title: body.title,
+      description: body.description,
+      completed: body.completed,
+      createdAt: new Date(),
+      priority: body.priority,
+    };
+
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(500).json("Post request unsuccessful", err);
+  }
+});
 // router.put("/:id");
 // router.delete("/:id");
 // router.patch("/:id/toggle");
